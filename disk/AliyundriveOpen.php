ae0d2a3e379f9035d649b5eb22b51d769d2b361c
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2024-01-30 17:26:23 +0800

    Update AliyundriveOpen.php

--- disk/AliyundriveOpen.php
+++ disk/AliyundriveOpen.php
@@ -833,7 +833,7 @@ class AliyundriveOpen {
                     $tmp = curl('POST', $this->my_oauth_url . 'access_token',  json_encode($data), ["Content-type" => "application/json"]);
                 } else {
                     $title = getconstStr('Wait');
-                    $html = '<button onclick="location.href = location.href;">' . getconstStr('Refresh') . '</button>';
+                    $html = '<button onclick="this.style.disabled = 1; location.href = location.href;">' . getconstStr('Refresh') . '</button>';
                     return message($html, $title, 400);
                 }
             }
